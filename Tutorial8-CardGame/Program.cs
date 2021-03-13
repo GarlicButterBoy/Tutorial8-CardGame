a7d5bfb02b6ddd2f6f2e28fb24fe363b7a4e7d8a
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-03-13 12:11:14 -0500

    Add project files.

---
+++ Tutorial8-CardGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial8_CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
